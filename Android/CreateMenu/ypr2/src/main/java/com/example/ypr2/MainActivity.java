face0a5121fb83ee83c4f3728a5bc4e1e90b0400
Dimidroll <JiJay.mr@mail.ru>
2023-02-19 15:02:34 +0600

    Finished 2/5

--- Android/CreateMenu/ypr2/src/main/java/com/example/ypr2/MainActivity.java
+++ Android/CreateMenu/ypr2/src/main/java/com/example/ypr2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ypr2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
