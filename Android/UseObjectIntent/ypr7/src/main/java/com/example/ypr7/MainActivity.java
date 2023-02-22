e90d505a34bf90a38a4b8bd578e7ec4520674d62
Dimidroll <JiJay.mr@mail.ru>
2023-02-22 19:35:17 +0600

    Finished 7/7

---
+++ Android/UseObjectIntent/ypr7/src/main/java/com/example/ypr7/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ypr7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Intent intentCall = new Intent(Intent.ACTION_DIAL);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(intentCall);
+    }
+}
