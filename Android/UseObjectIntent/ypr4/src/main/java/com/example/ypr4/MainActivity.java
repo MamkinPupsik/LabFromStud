97a676dce399ee46df59216af85d5a62d2e85cb3
Dimidroll <JiJay.mr@mail.ru>
2023-02-22 12:26:33 +0600

    Finished 4/7

---
+++ Android/UseObjectIntent/ypr4/src/main/java/com/example/ypr4/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ypr4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final static String ACTION_VIEW_CONTACTS = "VIEW_CONTACTS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(ACTION_VIEW_CONTACTS));
+    }
+}
