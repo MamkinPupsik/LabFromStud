880efc3238e4bf73c14752cdbb31c94d4c63a95f
Dimidroll <JiJay.mr@mail.ru>
2023-02-26 14:29:49 +0600

    Finished 3/4

--- Android/Fragments/ypr3/src/main/java/com/example/ypr3/MainActivity.java
+++ Android/Fragments/ypr3/src/main/java/com/example/ypr3/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.ypr3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
     }
+
+    public void onClick(View view){
+        fragmentTransaction = fragmentManager.beginTransaction();
+        switch (view.getId()){
+            case R.id.button1:
+                Fragment1 fragment1 = new Fragment1();
+                fragmentTransaction.add(R.id.fragmentLayout, fragment1);
+                break;
+            case R.id.button2:
+                Fragment2 fragment2 = new Fragment2();
+                fragmentTransaction.add(R.id.fragmentLayout, fragment2);
+                break;
+        }
+        fragmentTransaction.commit();
+    }
+
 }
